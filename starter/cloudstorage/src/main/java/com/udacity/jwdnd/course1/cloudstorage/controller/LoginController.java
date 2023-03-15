c1862eab71afa60084b5b54c9c2f9f0d34ea1c49
PHadaia <philipp.hadaia@outlook.de>
2023-03-16 00:12:37 +0100

    Update Login ModelAttributes

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.service.AuthenticationService;
 import com.udacity.jwdnd.course1.cloudstorage.service.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/login")
 public class LoginController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
-    public LoginController(UserService userService) {
+    public LoginController(UserService userService, AuthenticationService authenticationService) {
         this.userService = userService;
+        this.authenticationService = authenticationService;
     }
 
     @GetMapping()
-    public String loginView() {
+    public String loginView(@ModelAttribute("user") User user) {
         return "login";
     }
 
     @PostMapping()
-    public String loginUser(@ModelAttribute User user, Model model) {
+    public String loginUser(@ModelAttribute("user") User user, Model model, Authentication authentication) {
         String loginError = null;
 
-        //if(!userService)
+        if(userService.getUserByUsername(user.getUsername()) != null) {
+            authenticationService.authenticate(authentication);
+        }
         return "login";
     }
 }
