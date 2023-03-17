ebf516e551472d3dd3eea96b7e46b2034fdf7318
PHadaia <philipp.hadaia@outlook.de>
2023-03-17 22:50:39 +0100

    Credentials FIXED

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/CredentialService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/CredentialService.java
@@ -4,21 +4,40 @@ import com.udacity.jwdnd.course1.cloudstorage.model.Credentials;
 import com.udacity.jwdnd.course1.cloudstorage.repository.CredentialMapper;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.List;
 
 @Service
 public class CredentialService {
     private final CredentialMapper credentialMapper;
+    private final EncryptionService encryptionService;
 
-    public CredentialService(CredentialMapper credentialMapper) {
+    public CredentialService(CredentialMapper credentialMapper, EncryptionService encryptionService) {
         this.credentialMapper = credentialMapper;
+        this.encryptionService = encryptionService;
     }
     public int createCredentials(Credentials credentials) {
-        return credentialMapper.insertCredentials(credentials);
+        return credentialMapper.insertCredentials(encryptPassword(credentials));
+    }
+
+    public Credentials encryptPassword(Credentials credentials) {
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        random.nextBytes(key);
+        String encodedKey = Base64.getEncoder().encodeToString(key);
+        String encryptedPassword = encryptionService.encryptValue(credentials.getPassword(), encodedKey);
+        credentials.setKey(encodedKey);
+        credentials.setPassword(encryptedPassword);
+        return credentials;
+    }
+
+    public String decryptPassword(String encryptedPassword, String key) {
+        return encryptionService.decryptValue(encryptedPassword, key);
     }
 
     public boolean updateCredentials(Credentials credentials) {
-        return credentialMapper.updateCredentials(credentials);
+        return credentialMapper.updateCredentials(encryptPassword(credentials));
     }
 
     public List<Credentials> getCredentials(Integer userId) {
